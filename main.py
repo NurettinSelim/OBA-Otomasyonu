61cd2c9039238409a8177629294bb077e66b57cf
NurettinSelim <30979744+NurettinSelim@users.noreply.github.com>
2022-07-27 19:15:14 +0300

    calculates wait_minute

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ driver.add_cookie({"name": "PHPSESSID", "value": "SECRET"})
 
 while True:
     try:
+        wait_second = 1500
         driver.get(video_url)
         time.sleep(5)
         items = driver.find_elements(By.CLASS_NAME, "course-player-object-list-item")
@@ -23,11 +24,16 @@ while True:
             icon = item.find_element(By.CLASS_NAME, "mdi")
             if icon.get_attribute("class").find("mdi-circle-slice-3") > 0:
                 print(f"{item.text} dersi izlenmeye başlandı.")
+                text = item.text
+                wait_second = int(text[text.find("(") + 1:text.find("dk")]) * 60
+                wait_second += int(text[text.find("dk ") + 3:text.find("sn")])
+                # for network connection stability :)
+                wait_second += 60
 
         play_button = driver.find_element(By.XPATH, '//*[@id="video"]/button')
         play_button.click()
 
-        time.sleep(1500)
+        time.sleep(wait_second)
 
     except Exception as e:
         print("Bir hata oluştu, tekrar deneniyor.")
