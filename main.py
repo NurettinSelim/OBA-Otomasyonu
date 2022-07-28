e77b61087935bb240c758ffa55a41e1594270d3f
NurettinSelim <30979744+NurettinSelim@users.noreply.github.com>
2022-07-28 20:32:19 +0300

    login for cookies added

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import time
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+cookie_driver = webdriver.Edge()
+oba_login_url = "https://mebbisyd.meb.gov.tr/ssologinBIDB.aspx?id=72&URL=http://giris.eba.gov.tr/EBA_GIRIS/GirisKontrol/oba"
+phpsessid_cookie = ""
+cookie_driver.get(oba_login_url)
+
+print("Giriş yapmanız bekleniyor. Giriş yaptıktan sonra sekme otomatik olarak kapanacaktır.")
+
+while len(phpsessid_cookie) <= 0:
+    for i in cookie_driver.get_cookies():
+        if i.get("name") == "PHPSESSID":
+            phpsessid_cookie = i.get("value")
+    time.sleep(2)
+
+cookie_driver.close()
+print("Giriş başarılı!")
 
 options = webdriver.EdgeOptions()
 options.headless = True
@@ -12,7 +29,7 @@ video_url = "https://www.oba.gov.tr/egitim/oynatma/uzman-ogretmenlik-egitim-prog
 main_url = "https://www.oba.gov.tr/"
 
 driver.get(main_url)
-driver.add_cookie({"name": "PHPSESSID", "value": "SECRET"})
+driver.add_cookie({"name": "PHPSESSID", "value": phpsessid_cookie})
 
 while True:
     try:
@@ -23,7 +40,7 @@ while True:
         for item in items:
             icon = item.find_element(By.CLASS_NAME, "mdi")
             if icon.get_attribute("class").find("mdi-circle-slice-3") > 0:
-                print(f"{item.text} dersi izlenmeye başlandı.")
+                print(f"[{datetime.now()}] {item.text} dersi izlenmeye başlandı.")
                 text = item.text
                 wait_second = int(text[text.find("(") + 1:text.find("dk")]) * 60
                 wait_second += int(text[text.find("dk ") + 3:text.find("sn")])
@@ -36,5 +53,5 @@ while True:
         time.sleep(wait_second)
 
     except Exception as e:
-        print("Bir hata oluştu, tekrar deneniyor.")
+        print(f"[{datetime.now()}] Bir hata oluştu, tekrar deneniyor.")
         print(f"Hata kodu: {e}")
